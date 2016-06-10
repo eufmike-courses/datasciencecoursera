d045b52c55bf5e6f03399db27aea718ed7334633
eufmike <sc2.shih@gmail.com>
2016-06-09 22:37:11 -0400

    generate three chart

--- 09_Developing Data Products/Ichiro_Performance/dataprocessing.R
+++ 09_Developing Data Products/Ichiro_Performance/dataprocessing.R
@@ -4,73 +4,61 @@ library(gridExtra)
 library(dplyr)
 library(rCharts)
 
-# import data from 1871 to 2015
-Master <- read.csv("./data/2015/Master.csv")
-Batting <- read.csv("./data/2015/Batting.csv")
+# # Create a complied batting dataset with the name of player, the birth year
+# # (for age calculation) and the cumulative hit record.
+# 
+# # import data from 1871 to 2015
+# Master <- read.csv("./data/2015/Master.csv")
+# Batting <- read.csv("./data/2015/Batting.csv")
+# 
+# # combine data year of birth with batting data sheet
+# Master.year <- Master[, c("playerID", "birthYear", "nameFirst", "nameLast", "nameGiven")]
+# Batting.year <- merge(Batting, Master.year)
+# Batting.2 <- tbl_df(Batting.year)
+# Batting.2 <- Batting.2 %>% arrange(., yearID)
+# 
+# # Generate cumulative hit data
+# for (i in unique(Batting.2$playerID)){
+#         Batting.2$H_cum[Batting.2$playerID == i] <- cumsum(Batting.2$H[Batting.2$playerID == i])
+#         Batting.2$firstyear[Batting.2$playerID == i] <- min(Batting.2$yearID[Batting.2$playerID == i])
+# 
+# }
+# write.csv(Batting.2, "./data/Batting_Master.csv")
 
-# combine data year of birth with batting data sheet
-Master.year <- Master[, c("playerID", "birthYear", "nameFirst", "nameLast", "nameGiven")]
-Batting.year <- merge(Batting, Master.year)
-Batting.2 <- tbl_df(Batting.year)
+
+
+Batting.data <- read.csv("./data/Batting_Master.csv")
 
 # create two columns: batting average and age
-Batting.2 <- Batting.2 %>% 
+Batting.2 <- Batting.data %>% 
         mutate(., battingaverage = H/AB) %>%
         mutate(., age = yearID - birthYear) %>%
+        mutate(., year.in.MLB = yearID - firstyear + 1 ) %>%
         mutate(., nameFull = paste(nameGiven, nameLast)) 
 
-summary(Batting.2)
-dim(Batting.2)
+hit.perfomance <- Batting.2 %>%
+                group_by(., playerID) %>%
+                summarise(., total_hit = sum(H, na.rm = T)) %>%
+                filter(., total_hit > 2500)
 
-# remove NA value in year and batting average
-Batting.2 <- Batting.2[!is.na(Batting.2$battingaverage), ]
-Batting.2 <- Batting.2[!is.na(Batting.2$birthYear), ]
-summary(Batting.2)
-dim(Batting.2)
+Batting.all.total <- Batting.2[Batting.2$playerID %in% hit.perfomance$playerID, ]
 
-# check why the age data
-hist(Batting.2$age)
-table(Batting.2$age)
 
-# remove the error of negative age
-Batting.2$age[Batting.2$age <0]
-which(Batting.2$age < 0)
-Batting.2[which(Batting.2$age < 0), c("nameFirst", "nameLast", "nameGiven", "yearID", "birthYear")]
-Batting.2$birthYear[grepl("William F.", Batting.2$nameGiven) & 
-                            grepl("Johnson", Batting.2$nameLast)] <- 1862
+battingplot <- function(data, playerID, x, y){
+                Batting.playerID <- data[data$playerID == playerID, ]
+                p <- ggplot(data, aes_string(x = x, y = y, group = "nameFull")) + 
+                        geom_line() +
+                        geom_point() +  
+                        geom_point(data = Batting.playerID, 
+                                   aes_string(x = x, y = y), color = "red") + 
+                        geom_line(data = Batting.playerID, 
+                                  aes_string(x = x, y = y), color = "red")
+                return(p)
+                }
 
-# check the negative age are removed
-hist(Batting.2$age)
-table(Batting.2$age)
-
-# merge name to nameFull
-Batting.2 <- Batting.2 %>% 
-                mutate(., age = yearID - birthYear) 
-head(Batting.2)
-
-# Batting average whole life
-Batting.life <- Batting.2 %>%
-        group_by(., playerID) %>%
-        summarise(., avg = mean(battingaverage, na.rm = T),
-                  total.AB = sum(AB, na.rm = T)) %>%
-        filter(., total.AB >= 500) %>%
-        arrange(., desc(avg))
-
-head(Batting.life)
-Batting.new <- Batting.2[Batting.2$playerID %in% Batting.life$playerID, ]
-
-Batting.new[Batting.new$age == max(Batting.new$age), c("playerID", "yearID","H", "battingaverage", "age")]
-
-p1 <- ggplot(Batting.new, aes(x = age, y = battingaverage)) + geom_point()
+p1 <- battingplot(Batting.all.total, "suzukic01", "yearID", "H_cum")
 p1
-
-# Generate cumulative hit data
-Batting.3 <- Batting
-for (i in unique(Batting.3$playerID)){
-        Batting.3$H_cum[Batting.3$playerID == i] <- cumsum(Batting.3$H[Batting.3$playerID == i])
-        
-}
-
-p3 <- ggplot(Batting.3, aes(x = yearID, y = H_cum)) + geom_point()
+p2 <- battingplot(Batting.all.total, "suzukic01", "age", "H_cum")
+p2
+p3 <- battingplot(Batting.all.total, "suzukic01", "year.in.MLB", "H_cum")
 p3
-
